e3a4e8047b0d88697b84b5214525f540ff3e013b
Roel Delos Reyes <badcoder28@gmail.com>
2018-06-24 18:19:27 +0800

    Add configuration of flask mail using sendgrid and setting redis memory database

--- src/config.py
+++ src/config.py
@@ -1,18 +1,43 @@
+import os
+from pathlib import Path
+
 from flask import Flask
+from flask_mail import Mail, Message
+from redis import Redis
+
+from dotenv import load_dotenv
 from task import make_celery
 
-def initialize_app_configuration():
 
-    # Initialize flask application 
+env_path = Path('..') / '.env'
+load_dotenv(dotenv_path=str(env_path))
+
+
+def app_setting():
+
+    # Initialize flask application
     app = Flask(__name__)
 
     # Add celery server backend on top of redis
     app.config.update(
-        CELERY_BROKER_URL='redis://localhost:6379',
-        CELERY_RESULT_BACKEND='redis://localhost:6379'
+        CELERY_BROKER_URL=str(os.getenv('REDIS_BROKER')),
+        CELERY_RESULT_BACKEND=str(os.getenv('REDIS_BACKEND')),
+    )
+
+    # Add SendGrid configuration API
+    app.config.update(
+        MAIL_SERVER='smtp.sendgrid.com',
+        MAIL_PORT=587,
+        MAIL_USE_TLS=True,
+        MAIL_USERNAME=os.getenv('SG_USERNAME'),
+        MAIL_PASSWORD=os.getenv('SG_PASSWORD'),
+        MAIL_DEFAULT_SENDER=os.getenv('DEFAULT_MAIL'),
     )
 
     return app
 
-flask_app = initialize_app_configuration()
+
+flask_app = app_setting()
+redis_db = Redis(host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT'))
 celery = make_celery(flask_app)
+mail = Mail(flask_app)
